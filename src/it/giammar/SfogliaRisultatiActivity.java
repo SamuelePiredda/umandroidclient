36d1e0d00f683490c214f5e754c340a37d9e4ef6
rc1433 <rc1433>
2012-03-04 21:47:11 +0000

    Quasi agganciato al codice di collegamento backend.

--- src/it/giammar/SfogliaRisultatiActivity.java
+++ src/it/giammar/SfogliaRisultatiActivity.java
@@ -1,18 +1,44 @@
 package it.giammar;
 
+import static org.fusesource.stomp.client.Constants.DESTINATION;
+import static org.fusesource.stomp.client.Constants.ID;
+import static org.fusesource.stomp.client.Constants.MESSAGE_ID;
+import static org.fusesource.stomp.client.Constants.SEND;
+import static org.fusesource.stomp.client.Constants.SUBSCRIBE;
+import it.giammar.pratomodel.QueryReply;
+import it.giammar.pratomodel.QueryReply.Database;
+import it.giammar.pratomodel.QueryRequest;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.fusesource.hawtbuf.AsciiBuffer;
+import org.fusesource.stomp.client.BlockingConnection;
+import org.fusesource.stomp.client.Stomp;
+import org.fusesource.stomp.codec.StompFrame;
+
 import android.app.Activity;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.StaxDriver;
 
 public class SfogliaRisultatiActivity extends Activity {
 	private static final int SWIPE_MIN_DISTANCE = 120;
@@ -25,18 +51,19 @@ public class SfogliaRisultatiActivity extends Activity {
 	private Animation slideRightIn;
 	private Animation slideRightOut;
 	private ViewFlipper viewFlipper;
+	private QueryRequest qr;
+	LayoutInflater inflater;
+	private Activity io = this;
+	private Map<QueryReply.Database, LinearLayout> visBancheDati = new HashMap<QueryReply.Database, LinearLayout>();
 
-	// private Map<QueryReply.Database, ListView> tabelle = new
-	// HashMap<QueryReply.Database, ListView>();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		ListView listView = (ListView) inflater.inflate(R.layout.risultati,
-				null);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.sfogliarisultati);
+		XStream xstream = new XStream();
+		System.out.println(getIntent().getExtras().getString("query"));
+		inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper1);
-		String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-				"Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-				"Linux", "OS/2" };
 		slideLeftIn = AnimationUtils.loadAnimation(this,
 				android.R.anim.slide_in_left);
 		slideLeftOut = AnimationUtils.loadAnimation(this,
@@ -45,35 +72,6 @@ public class SfogliaRisultatiActivity extends Activity {
 				android.R.anim.slide_in_left);
 		slideRightOut = AnimationUtils.loadAnimation(this,
 				android.R.anim.slide_out_right);
-		// First paramenter - Context
-		// Second parameter - Layout for the row
-		// Third parameter - ID of the View to which the data is written
-		// Forth - the Array of data
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, android.R.id.text1, values);
-
-		// Assign adapter to ListView
-		listView.setAdapter(adapter);
-
-		ListView listView2 = (ListView) inflater.inflate(R.layout.risultati,
-				null);
-		String[] values2 = new String[] { "asdf", "cccc", "ccczxcvzxcv",
-				"zxcvz", "asdfasdfa", "asdfasdf", "adsfadsf",
-				"Max zxcvzxcvzxcv X", "zxb fg", "OSxcvzxcvzxcvzxcvzxcv2" };
-
-		// First paramenter - Context
-		// Second parameter - Layout for the row
-		// Third parameter - ID of the View to which the data is written
-		// Forth - the Array of data
-		ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, android.R.id.text1,
-				values2);
-
-		// Assign adapter to ListView
-		listView2.setAdapter(adapter2);
-		viewFlipper.addView(listView);
-
-		viewFlipper.addView(listView2);
 		gestureDetector = new GestureDetector(new MyGestureDetector());
 		gestureListener = new View.OnTouchListener() {
 			public boolean onTouch(View v, MotionEvent event) {
@@ -83,12 +81,166 @@ public class SfogliaRisultatiActivity extends Activity {
 				return false;
 			}
 		};
-		listView.setOnTouchListener(gestureListener);
-		listView2.setOnTouchListener(gestureListener);
-		super.onCreate(savedInstanceState);
+		viewFlipper.setOnTouchListener(gestureListener);
+
+		associaBancheDatiaViews();
+		riempiFlipperconBancheDati();
+
+		qr = (QueryRequest) xstream.fromXML(getIntent().getExtras().getString(
+				"query"));
+		new EffettuaQuery().execute(qr);
+
+		// ListView listView = (ListView) inflater.inflate(R.layout.risultati,
+		// null);
+		//
+		// String[] values = new String[] { "Android", "iPhone",
+		// "WindowsMobile",
+		// "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+		// "Linux", "OS/2" };
+		//
+		// // First paramenter - Context
+		// // Second parameter - Layout for the row
+		// // Third parameter - ID of the View to which the data is written
+		// // Forth - the Array of data
+		// ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+		// android.R.layout.simple_list_item_1, android.R.id.text1, values);
+		//
+		// // Assign adapter to ListView
+		// listView.setAdapter(adapter);
+		//
+		// ListView listView2 = (ListView) inflater.inflate(R.layout.risultati,
+		// null);
+		// String[] values2 = new String[] { "asdf", "cccc", "ccczxcvzxcv",
+		// "zxcvz", "asdfasdfa", "asdfasdf", "adsfadsf",
+		// "Max zxcvzxcvzxcv X", "zxb fg", "OSxcvzxcvzxcvzxcvzxcv2" };
+		//
+		// // First paramenter - Context
+		// // Second parameter - Layout for the row
+		// // Third parameter - ID of the View to which the data is written
+		// // Forth - the Array of data
+		// ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
+		// android.R.layout.simple_list_item_1, android.R.id.text1,
+		// values2);
+		//
+		// // Assign adapter to ListView
+		// listView2.setAdapter(adapter2);
+		// viewFlipper.addView(listView);
+		//
+		// viewFlipper.addView(listView2);
+		//
+		// listView.setOnTouchListener(gestureListener);
+		// listView2.setOnTouchListener(gestureListener);
+		// super.onCreate(savedInstanceState);
+	}
+
+	private void riempiFlipperconBancheDati() {
+		for (Entry<Database, LinearLayout> es : visBancheDati.entrySet()) {
+			viewFlipper.addView(es.getValue());
+		}
+
+	}
+
+	private void associaBancheDatiaViews() {
+		LinearLayout l = (LinearLayout) inflater.inflate(R.layout.risultati,
+				null);
+		TextView t = (TextView) l.getChildAt(0);
+		ListView lv = (ListView) l.getChildAt(1);
+		lv.setOnTouchListener(gestureListener);
+		t.setText("ANIA");
+		visBancheDati.put(Database.ANIA, l);
+
+		l = (LinearLayout) inflater.inflate(R.layout.risultati, null);
+		t = (TextView) l.getChildAt(0);
+		lv = (ListView) l.getChildAt(1);
+		lv.setOnTouchListener(gestureListener);
+		t.setText("PRA");
+		visBancheDati.put(Database.PRA, l);
+
+		l = (LinearLayout) inflater.inflate(R.layout.risultati, null);
+		lv = (ListView) l.getChildAt(1);
+		lv.setOnTouchListener(gestureListener);
+		t = (TextView) l.getChildAt(0);
+		t.setText("RUBATI");
+		visBancheDati.put(Database.RUBATI, l);
+	}
+
+	private class EffettuaQuery extends
+			AsyncTask<QueryRequest, QueryReply, Void> {
+
+		@Override
+		protected Void doInBackground(QueryRequest... params) {
+			Stomp stomp;
+			try {
+				stomp = new Stomp("tcp://localhost:61613");
+
+				BlockingConnection connection = stomp.connectBlocking();
+
+				StompFrame frame = new StompFrame(SUBSCRIBE);
+				frame.addHeader(DESTINATION,
+						StompFrame.encodeHeader("/queue/test"));
+				frame.addHeader(ID, connection.nextId());
+				StompFrame response = connection.request(frame);
+
+				QueryRequest q = params[0];
+				XStream xstream = new XStream(new StaxDriver());
+
+				frame = new StompFrame(SEND);
+				frame.addHeader(DESTINATION,
+						StompFrame.encodeHeader("/queue/queryServer"));
+				frame.addHeader(MESSAGE_ID, StompFrame.encodeHeader("test"));
+				frame.addHeader(StompFrame.encodeHeader("stomp"),
+						StompFrame.encodeHeader("yes"));
+				frame.addHeader(
+						StompFrame.encodeHeader("CamelJmsDestinationName"),
+						StompFrame.encodeHeader("test"));
+				frame.content(new AsciiBuffer(xstream.toXML(q)));
+				connection.send(frame);
+				while (true) {
+					StompFrame received = connection.receive();
+					QueryReply qrep= (QueryReply) xstream.fromXML(received.contentAsString());
+					publishProgress(qrep);
+				}
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		@Override
+		protected void onProgressUpdate(QueryReply... values) {
+			// TODO Auto-generated method stub
+			super.onProgressUpdate(values);
+		}
+
+//		@Override
+//		protected void onPostExecute(Void result) {
+//			super.onPostExecute(result);
+//			String[] values = new String[] { "Android", "iPhone",
+//					"WindowsMobile", "Blackberry", "WebOS", "Ubuntu",
+//					"Windows7", "Max OS X", "Linux", "OS/2" };
+//
+//			// First paramenter - Context
+//			// Second parameter - Layout for the row
+//			// Third parameter - ID of the View to which the data is written
+//			// Forth - the Array of data
+//			ArrayAdapter<String> adapter = new ArrayAdapter<String>(io,
+//					android.R.layout.simple_list_item_1, android.R.id.text1,
+//					values);
+//
+//			// Assign adapter to ListView
+//			ListView x = (ListView) visBancheDati.entrySet().iterator().next()
+//					.getValue().getChildAt(1);
+//			x.setAdapter(adapter);
+//		}
+
 	}
 
 	class MyGestureDetector extends SimpleOnGestureListener {
+
 		@Override
 		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
 				float velocityY) {
@@ -114,14 +266,5 @@ public class SfogliaRisultatiActivity extends Activity {
 			return false;
 		}
 	}
-	// private void inizializzaTabelle() {
-	// tabelle.put(Database.ANIA, ania);
-	// // table_1.addContainerProperty("Risultato", RichTextArea.class, null);
-	// tabelle.put(Database.PRA, pra);
-	// tabelle.put(Database.RUBATI, rubati);
-	// for (Table tabella : tabelle.values()) {
-	// tabella.addContainerProperty("Chiave", RichTextArea.class, null);
-	// tabella.addContainerProperty("Valore", RichTextArea.class, null);
-	// }
-	// }
+
 }
--- src/it/giammar/UMAndroidClientActivity.java
+++ src/it/giammar/UMAndroidClientActivity.java
@@ -1,14 +1,24 @@
 package it.giammar;
 
+import it.giammar.pratomodel.QueryRequest;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 
 public class UMAndroidClientActivity extends Activity implements OnClickListener {
 	private Button cerca;
+	private EditText query;
+	private CheckBox aPagamento;
+	private Spinner bancaDati;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -16,6 +26,9 @@ public class UMAndroidClientActivity extends Activity implements OnClickListener
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		cerca = (Button) findViewById(R.id.cerca);
+		query = (EditText) findViewById(R.id.query);
+		aPagamento = (CheckBox) findViewById(R.id.aPagamento);
+		bancaDati = (Spinner) findViewById(R.id.bancaDati);
 		cerca.setOnClickListener(this);
 		connettiBackend();
 	}
@@ -27,24 +40,21 @@ public class UMAndroidClientActivity extends Activity implements OnClickListener
 
 	@Override
 	public void onClick(View v) {
-		inviaQuery();
-		puliciListe();
+		QueryRequest qr= new QueryRequest();
+		preparaQuery(qr);
 		Intent sr = new Intent(this, SfogliaRisultatiActivity.class);
+		XStream xstream = new XStream();
+		sr.putExtra("query", xstream.toXML(qr));
 		this.startActivity(sr);
 	}
 
-	private void puliciListe() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void inviaQuery() {
-		// TODO Auto-generated method stub
-		
+	private void preparaQuery(QueryRequest qr) {
+		qr.setQuery(query.getText().toString());
+		qr.setAutomatic(true);
 	}
 
 	
 
 	
 
-}
+} 	
