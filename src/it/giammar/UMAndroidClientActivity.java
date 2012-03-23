c18bf7edc2fa94a7eb4ecc624fefca6f73eb6294
rc1433 <rc1433>
2012-03-23 16:19:54 +0000

    aggiunte opzioni spinner

--- src/it/giammar/UMAndroidClientActivity.java
+++ src/it/giammar/UMAndroidClientActivity.java
@@ -50,15 +50,15 @@ public class UMAndroidClientActivity extends Activity implements
 
 	private void riempiOpzioniDiRicerca() {
 		opzDiRicerca = new HashMap<Database, Tipo[]>();
-		opzDiRicerca.put(Database.ANAGRAFE, new Tipo[] { Tipo.NC, Tipo.CF });
+		opzDiRicerca.put(Database.ANAGRAFE, new Tipo[] { Tipo.NC, Tipo.CF,Tipo.IND });
 		opzDiRicerca.put(Database.ANIA, new Tipo[] { Tipo.TARGA });
-		opzDiRicerca.put(Database.CACOMM, new Tipo[] {});
-		opzDiRicerca.put(Database.CARRABILI, new Tipo[] {});
-		opzDiRicerca.put(Database.MCTC, new Tipo[] {});
-		opzDiRicerca.put(Database.OTV, new Tipo[] {});
+		opzDiRicerca.put(Database.CACOMM, new Tipo[] {Tipo.NC,Tipo.CF,Tipo.IND,Tipo.GEN});
+		opzDiRicerca.put(Database.CARRABILI, new Tipo[] {Tipo.NC,Tipo.IND,Tipo.GEN});
+		opzDiRicerca.put(Database.MCTC, new Tipo[] {Tipo.CF,Tipo.TARGA,Tipo.PATENTE});
+		opzDiRicerca.put(Database.OTV, new Tipo[] {Tipo.IND,Tipo.GEN});
 		opzDiRicerca.put(Database.PRA, new Tipo[] { Tipo.TARGA });
-		opzDiRicerca.put(Database.RUBATI, new Tipo[] { Tipo.TARGA });
-		opzDiRicerca.put(Database.ZTL, new Tipo[] {});
+		opzDiRicerca.put(Database.RUBATI, new Tipo[] { Tipo.TARGA,Tipo.TELAIO });
+		opzDiRicerca.put(Database.ZTL, new Tipo[] {Tipo.NC,Tipo.CF,Tipo.TARGA,Tipo.PERM});
 	}
 
 	@Override
